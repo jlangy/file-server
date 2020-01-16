8493d86e6a66153e4f632013aca60d44185d2273
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-16 03:27:00 +0000

    print received data to clients disk

--- client.js
+++ client.js
@@ -1,8 +1,12 @@
 const net = require('net');
-const args = process.argv.slice(2,5);
+const args = process.argv.slice(2,6);
 const IP = args[0];
 const PORT = args[1];
-const fileName = args[2];
+const requestFilePath = args[2];
+const printFilePath = args[3];
+
+const fs = require('fs');
+
 
 const conn = net.createConnection({
   host: IP,
@@ -13,9 +17,14 @@ conn.setEncoding('utf8');
 
 conn.on('connect', () => {
   console.log(`connected on port ${PORT}...`)
-  conn.write(fileName); 
+  conn.write(requestFilePath); 
 });
 
 conn.on('data', (data) => {
-  console.log(data);
+  console.log(data, printFilePath);
+  fs.writeFile(printFilePath, data, (err) => {
+    if(err) throw err;
+    console.log('file saved to disk');
+    process.exit();
+  })
 })
