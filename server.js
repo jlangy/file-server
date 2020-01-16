20e5e59ce3307378524f842b3d43effa922d5fd9
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-16 03:11:13 +0000

    adds functionality for server to look for filename

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const net = require('net');
-
 const server = net.createServer();
+
+const fs = require('fs');
 
 
 server.on('connection', (client) => {
   client.setEncoding('utf8');
   
-  client.on('data', (data) => {
-    console.log(data);
+  client.on('data', (filename) => {
+    const filePath = 'serverDir/' + filename;
+    fs.exists(filePath, (exists) => {
+      if (exists){
+        console.log('found it!');
+      } else {
+        console.log('cant find the file!');
+      }
+    });
   });
 });
 
